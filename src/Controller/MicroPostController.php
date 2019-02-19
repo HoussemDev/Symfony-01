5707ec2e1d0897343cb1dd205a425eb687bbffa4
HoussemDev <houss.sword@gmail.com>
2019-02-19 22:00:04 +0100

    Voter and last module securing the app

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -13,6 +13,7 @@ use App\Entity\MicroPost;
 use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,6 +21,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 /**
  * Class MicroPostController
@@ -80,6 +82,7 @@ class MicroPostController
 
 	/**
 	 * @Route("/edit/{id}", name="micro_post_edit")
+	 * @Security(" is_granted('edit', microPost)", message="access denied")
 	 */
 	public function edit(MicroPost $microPost, Request $request)
 	{
@@ -102,11 +105,15 @@ class MicroPostController
 
 	/**
 	 * @Route("/add", name="micro_post_add")
+	 * @Security(" is_granted('ROLE_USER')")
 	 */
-	public function add(Request $request)
+	public function add(Request $request, TokenStorageInterface $tokenStorage)
 	{
+		$user = $tokenStorage->getToken()->getUser();
 		$microPost = new MicroPost();
 		$microPost->setTime(new \DateTime());
+		$microPost->setUser($user);
+
 		$form = $this->formFactory->create(MicroPostType::class, $microPost);
 		$form->handleRequest($request);
 
@@ -125,6 +132,7 @@ class MicroPostController
 
 	/**
 	 * @Route("/delete/{id}", name="micro_post_delete")
+	 * @Security(" is_granted('delete', microPost)", message="access denied")
 	 */
 	public function delete(MicroPost $microPost)
 	{
--- src/Security/MicroPostVoter.php
+++ src/Security/MicroPostVoter.php
@@ -12,13 +12,22 @@ namespace App\Security;
 use App\Entity\MicroPost;
 use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 class MicroPostVoter extends Voter
 {
 	const EDIT = 'edit';
 	const DELETE = 'delete';
+	/**
+	 * @var AccessDecisionManagerInterface
+	 */
+	private $decisionManager;
 
+	public function __construct(AccessDecisionManagerInterface $decisionManager)
+	{
+		$this->decisionManager = $decisionManager;
+	}
 
 	protected function supports($attribute, $subject)
 	{
@@ -36,6 +45,11 @@ class MicroPostVoter extends Voter
 
 	protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
 	{
+		if($this->decisionManager->decide($token, [User::ROLE_ADMIN]))
+		{
+			return true;
+		}
+
 		$authenticatedUser = $token->getUser();
 		if(!$authenticatedUser instanceof User)
 		{
